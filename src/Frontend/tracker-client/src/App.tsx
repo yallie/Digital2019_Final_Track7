4426fd8d734a02c187b27a744a7fdcfead512fab
yallie <yallie@yandex.ru>
2019-09-27 21:40:45 +0300

    Added the navigation.

--- src/Frontend/tracker-client/src/App.tsx
+++ src/Frontend/tracker-client/src/App.tsx
@@ -2,39 +2,48 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'antd/dist/antd.css';
+import { useState } from 'react';
 
 import { Layout, Menu, Breadcrumb } from 'antd';
 const { Header, Content, Footer } = Layout;
 
 const App: React.FC = () => {
-  return (
-	<Layout className="layout">
-		<Header>
-			<div className="logo" />
-			<Menu
-				theme="dark"
-				mode="horizontal"
-				defaultSelectedKeys={['2']}
-				style={{ lineHeight: '64px' }}
-			>
-				<Menu.Item key="1">nav 1</Menu.Item>
-				<Menu.Item key="2">nav 2</Menu.Item>
-				<Menu.Item key="3">nav 3</Menu.Item>
-			</Menu>
-		</Header>
 
-		<Content style={{ padding: '0 50px' }}>
-			<Breadcrumb style={{ margin: '16px 0' }}>
-				<Breadcrumb.Item>Home</Breadcrumb.Item>
-				<Breadcrumb.Item>List</Breadcrumb.Item>
-				<Breadcrumb.Item>App</Breadcrumb.Item>
-			</Breadcrumb>
-			<div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Content</div>
-		</Content>
+	const [page, setPage] = useState("main")
+	const getSetPage = (page: string) => () => setPage(page)
 
-		<Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-	</Layout>
-  );
+	return (
+		<Layout className="layout">
+			<Header>
+				<div className="logo" />
+				<Menu
+					theme="dark"
+					mode="horizontal"
+					selectedKeys={[page]}
+					style={{ lineHeight: '64px' }}
+				>
+					<Menu.Item key="main" onClick={getSetPage("main")}>Главная</Menu.Item>
+					<Menu.Item key="tasks" onClick={getSetPage("tasks")}>Задания такелажнику</Menu.Item>
+					<Menu.Item key="map" onClick={getSetPage("map")}>Администратор</Menu.Item>
+				</Menu>
+			</Header>
+
+			<Content style={{ padding: '0 50px' }}>
+				<Breadcrumb style={{ margin: '16px 0' }}>
+					<Breadcrumb.Item>Home</Breadcrumb.Item>
+					<Breadcrumb.Item>List</Breadcrumb.Item>
+					<Breadcrumb.Item>App</Breadcrumb.Item>
+				</Breadcrumb>
+
+				<div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+					{page}
+				</div>
+
+			</Content>
+
+			<Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+		</Layout>
+	);
 }
 
 export default App;
