4fd8ed5b360f96fb98de7b073cf9f3ae3b7add2a
yallie <yallie@yandex.ru>
2019-09-27 19:40:22 +0300

    Added NancyFX backend template.

---
+++ src/Backend/MainModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+
+namespace Track7
+{
+	public class MainModule : NancyModule
+	{
+		public MainModule()
+		{
+			Get("/", _ => "Hello World!");
+		}
+	}
+}
---
+++ src/Backend/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy.Hosting.Self;
+
+namespace Track7
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// initialize an instance of NancyHost (found in the Nancy.Hosting.Self package)
+			var host = new NancyHost(new Uri("http://localhost:37000"));
+			host.Start(); // start hosting
+
+			Console.ReadKey();
+			host.Stop();  // stop hosting
+		}
+	}
+}
