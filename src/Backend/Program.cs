2e2a9d09b4b90b73c22d3c9fc16d8a10d121bddc
yallie <yallie@yandex.ru>
2019-09-27 19:58:37 +0300

    Added automatic URL reservation.

--- src/Backend/Program.cs
+++ src/Backend/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Nancy;
 using Nancy.Hosting.Self;
 
 namespace Track7
@@ -11,8 +12,11 @@ namespace Track7
 	{
 		static void Main(string[] args)
 		{
+			var hostConfigs = new HostConfiguration();
+			hostConfigs.UrlReservations.CreateAutomatically = true;
+
 			// initialize an instance of NancyHost (found in the Nancy.Hosting.Self package)
-			var host = new NancyHost(new Uri("http://localhost:37000"));
+			var host = new NancyHost(new Uri("http://localhost:37000"), new DefaultNancyBootstrapper(), hostConfigs);
 			host.Start(); // start hosting
 
 			Console.ReadKey();
