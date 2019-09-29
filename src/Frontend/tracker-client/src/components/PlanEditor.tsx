7b3e97dea0c6619e5668fb58c7e2d750f42f2b69
yallie <yallie@yandex.ru>
2019-09-29 09:16:01 +0300

    Trying to integrate react-designer again.

--- src/Frontend/tracker-client/src/components/PlanEditor.tsx
+++ src/Frontend/tracker-client/src/components/PlanEditor.tsx
@@ -1,21 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
 
-import Designer, { Rectangle } from 'react-designer'
-
 export default function PlanEditor() {
-    const [objects, setObjects] = useState([
-        {type: "rect", x: 50, y: 70, fill: "red"}
-      ] as any[])
-
-    return (
-        <Designer width={250} height={350}
-            objectTypes={{
-                'text': Text,
-                'rect': Rectangle
-            }}
-            onUpdate={(objects: any[]) => setObjects(objects)}
-            objects={objects}
-        />
-    )
+    return <p>"Stub"</p>
 }
